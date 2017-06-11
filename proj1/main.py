Fix three_circle calculation problem

proj1/main.py
@@ -5,6 +5,7 @@ from scipy.optimize import fsolve
 import numpy as np
 import math
 import turtle
+import canvasvg
 
 
 class Point(object):
@@ -72,31 +73,52 @@ def two_circle_one_edge(circle1, circle2, edge):
     return Circle(x, y, r)
 
 
+# def three_circle(circle1, circle2, circle3):
+#     x1, y1, r1 = circle1.x, circle1.y, circle1.r
+#     x2, y2, r2 = circle2.x, circle2.y, circle2.r
+#     x3, y3, r3 = circle3.x, circle3.y, circle3.r
+#
+#     a = np.array([
+#         [x1 - x2, y1 - y2, r1 - r2],
+#         [x2 - x3, y2 - y3, r2 - r3],
+#         [x1 - x3, y1 - y3, r1 - r3]
+#     ])
+#     b = np.array([
+#         1 / 2 * (r2 * r2 - r1 * r1 + y1 * y1 - y2 * y2 + x1 * x1 - x2 * x2),
+#         1 / 2 * (r3 * r3 - r2 * r2 + y2 * y2 - y3 * y3 + x2 * x2 - x3 * x3),
+#         1 / 2 * (r2 * r1 - r1 * r1 + y1 * y1 - y3 * y3 + x1 * x1 - x3 * x3),
+#     ])
+#     x, y, r = np.linalg.solve(a, b)
+#     return Circle(x, y, r)
+
 def three_circle(circle1, circle2, circle3):
     x1, y1, r1 = circle1.x, circle1.y, circle1.r
     x2, y2, r2 = circle2.x, circle2.y, circle2.r
     x3, y3, r3 = circle3.x, circle3.y, circle3.r
 
-    a = np.array([
-        [x1 - x2, y1 - y2, r1 - r2],
-        [x2 - x3, y2 - y3, r2 - r3],
-        [x1 - x3, y1 - y3, r1 - r3]
-    ])
-    b = np.array([
-        1 / 2 * (r2 * r2 - r1 * r1 + y1 * y1 - y2 * y2 + x1 * x1 - x2 * x2),
-        1 / 2 * (r3 * r3 - r2 * r2 + y2 * y2 - y3 * y3 + x2 * x2 - x3 * x3),
-        1 / 2 * (r2 * r1 - r1 * r1 + y1 * y1 - y3 * y3 + x1 * x1 - x3 * x3),
-    ])
-    x, y, r = np.linalg.solve(a, b)
+    def equations(p):
+        x, y, r = p
+        return (pow(x - x1, 2) + pow(y - y1, 2) - pow(r + r1, 2),
+                pow(x - x2, 2) + pow(y - y2, 2) - pow(r + r2, 2),
+                pow(x - x3, 2) + pow(y - y3, 2) - pow(r + r3, 2))
+
+    c1, c2, c3 = circle1, circle2, circle3
+    # 将 c1, c2, c3 从小到大排列
+    if c1.r > c2.r:
+        c1, c2 = c2, c1
+    if c2.r > c3.r:
+        c2, c3 = c3, c2
+
+    x, y, r = fsolve(equations, (((c1.x + c2.x) / 2), (c1.y + c2.y) / 2, (c1.r + c2.r) / 2))
     return Circle(x, y, r)
 
 
 class Area(object):
-    def __init__(self, type, edges, circles):
-        self.type = type
+    def __init__(self, atype, edges, circles):
+        self.atype = atype
         self.edges = edges
         self.circles = circles
-        self.circle = None
+        self.in_circle = None
 
     class Type(object):
         ONE_CIRCLE_TWO_EDGE = 0
@@ -104,29 +126,28 @@ class Area(object):
         THREE_CIRCLE = 3
 
     def inner_circle(self):
-        if self.circle == None:
-            if self.type == Area.Type.ONE_CIRCLE_TWO_EDGE:
-                self.circle = one_circle_two_edge(self.circles[0], self.edges[0], self.edges[1])
-            elif self.type == Area.Type.TWO_CIRCLE_ONE_EDGE:
-                self.circle = two_circle_one_edge(self.circles[0], self.circles[1], self.edges[0])
-            elif self.type == Area.Type.THREE_CIRCLE:
-                self.circle = three_circle(self.circles[0], self.circles[1], self.circles[2])
-        return self.circle
+        if self.in_circle is None:
+            if self.atype == Area.Type.ONE_CIRCLE_TWO_EDGE:
+                self.in_circle = one_circle_two_edge(self.circles[0], self.edges[0], self.edges[1])
+            elif self.atype == Area.Type.TWO_CIRCLE_ONE_EDGE:
+                self.in_circle = two_circle_one_edge(self.circles[0], self.circles[1], self.edges[0])
+            elif self.atype == Area.Type.THREE_CIRCLE:
+                self.in_circle = three_circle(self.circles[0], self.circles[1], self.circles[2])
+        return self.in_circle
 
     def new_areas(self, inner_circle):
-        if self.type == Area.Type.ONE_CIRCLE_TWO_EDGE:
-            return [Area(Area.Type.ONE_CIRCLE_TWO_EDGE, self.edges, [inner_circle]),
-                    Area(Area.Type.TWO_CIRCLE_ONE_EDGE, [self.edges[0]], [self.circles[0], inner_circle]),
-                    Area(Area.Type.TWO_CIRCLE_ONE_EDGE, [self.edges[1]], [self.circles[0], inner_circle])]
-
-        elif self.type == Area.Type.TWO_CIRCLE_ONE_EDGE:
-            return [Area(Area.Type.THREE_CIRCLE, None, [self.circles[0], self.circles[1], inner_circle]),
-                    Area(Area.Type.TWO_CIRCLE_ONE_EDGE, [self.edges[0]], [self.circles[0], inner_circle]),
-                    Area(Area.Type.TWO_CIRCLE_ONE_EDGE, [self.edges[0]], [self.circles[1], inner_circle])]
-        elif self.type == Area.Type.THREE_CIRCLE:
-            return [Area(Area.Type.THREE_CIRCLE, None, [self.circles[0], self.circles[1], inner_circle]),
-                    Area(Area.Type.THREE_CIRCLE, None, [self.circles[0], self.circles[2], inner_circle]),
-                    Area(Area.Type.THREE_CIRCLE, None, [self.circles[1], self.circles[2], inner_circle])]
+        if self.atype == Area.Type.ONE_CIRCLE_TWO_EDGE:
+            return [Area(Area.Type.ONE_CIRCLE_TWO_EDGE, self.edges, (inner_circle,)),
+                    Area(Area.Type.TWO_CIRCLE_ONE_EDGE, (self.edges[0],), (self.circles[0], inner_circle)),
+                    Area(Area.Type.TWO_CIRCLE_ONE_EDGE, (self.edges[1],), (self.circles[0], inner_circle))]
+        elif self.atype == Area.Type.TWO_CIRCLE_ONE_EDGE:
+            return [Area(Area.Type.THREE_CIRCLE, (), (self.circles[0], self.circles[1], inner_circle)),
+                    Area(Area.Type.TWO_CIRCLE_ONE_EDGE, (self.edges[0],), (self.circles[0], inner_circle)),
+                    Area(Area.Type.TWO_CIRCLE_ONE_EDGE, (self.edges[0],), (self.circles[1], inner_circle))]
+        elif self.atype == Area.Type.THREE_CIRCLE:
+            return [Area(Area.Type.THREE_CIRCLE, (), (self.circles[0], self.circles[1], inner_circle)),
+                    Area(Area.Type.THREE_CIRCLE, (), (self.circles[0], self.circles[2], inner_circle)),
+                    Area(Area.Type.THREE_CIRCLE, (), (self.circles[1], self.circles[2], inner_circle))]
 
 
 def plot(result):
@@ -140,11 +161,15 @@ def plot(result):
     turtle.forward(600)  # 左下角
     turtle.right(90)  # 向上
     turtle.forward(600)  # 左上角
+    print "len = ", len(result)
     for circle in result:
+        print circle.x, circle.y, circle.r
         turtle.penup()
-        turtle.goto((circle.x + circle.r) * 300, circle.y)
+        turtle.goto((circle.x + circle.r) * 300, circle.y * 300)
         turtle.pendown()
         turtle.circle(circle.r * 300)
+    ts = turtle.getscreen().getcanvas()
+    canvasvg.saveall("result.svg", ts)
     turtle.exitonclick()
 
 
@@ -157,11 +182,11 @@ def main(m):
     edge3 = Edge(Point(-1, 1), Point(-1, -1))
     edge4 = Edge(Point(-1, -1), Point(1, -1))
     result.append(circle0)
-    queue.append(Area(Area.Type.ONE_CIRCLE_TWO_EDGE, [edge1, edge2], [circle0]))
-    queue.append(Area(Area.Type.ONE_CIRCLE_TWO_EDGE, [edge2, edge3], [circle0]))
-    queue.append(Area(Area.Type.ONE_CIRCLE_TWO_EDGE, [edge3, edge4], [circle0]))
-    queue.append(Area(Area.Type.ONE_CIRCLE_TWO_EDGE, [edge4, edge1], [circle0]))
-    while len(queue) > 0 and m >= 0:
+    queue.append(Area(Area.Type.ONE_CIRCLE_TWO_EDGE, (edge1, edge2), (circle0,)))
+    queue.append(Area(Area.Type.ONE_CIRCLE_TWO_EDGE, (edge2, edge3), (circle0,)))
+    queue.append(Area(Area.Type.ONE_CIRCLE_TWO_EDGE, (edge3, edge4), (circle0,)))
+    queue.append(Area(Area.Type.ONE_CIRCLE_TWO_EDGE, (edge4, edge1), (circle0,)))
+    while len(queue) > 0 and m > 0:
         area = queue.pop()
         circle = area.inner_circle()
         for i in range(0, len(queue)):  # 选出圆面积最大的区域